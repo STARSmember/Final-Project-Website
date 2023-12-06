4bb0ebb3ef970512ff0583be33dd2e3b280bc8ce
Julian Fragoso <starsmember@yahoo.com>
2023-12-06 00:17:32 -0600

    added villain variable

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -1,73 +1,65 @@
 <script setup>
-import { ref } from 'vue'
-import { faker } from '@faker-js/faker'
+  import { ref } from 'vue'
+  import { faker } from '@faker-js/faker'
 
-import useAPI from '@/composables/useAPI'
-const { getDepartment } = useAPI()
+  import useAPI from '@/composables/useAPI'
+  //const { getVillains } = useAPI()
 
-const selectCard = () => {
-  console.log(`${props.employee.name} selected`)
-}
+  const selectCard = () => {
+    console.log(`${props.villains.name} selected`)
+  }
 
-const props = defineProps({
-  employee: {
-    type: Object,
-    required: true,
-    default: () => {
-      return {
-        createdAt: '2022-01-01',
-        departmentId: '123',
-        email: 'john.doe@example.com',
-        employeeId: '123',
-        name: 'John Doe',
-        quote: 'Really Cool quote',
-        title: 'Position',
-        updatedAt: '2022-01-01',
-      }
+  const props = defineProps({
+    villain: {
+      type: Object,
+      required: true,
+      default: () => {
+        return {
+          villainId: '123',
+          realName: 'John Doe',
+          image: 'https://www.example.com'
+        }
+      },
     },
-  },
-})
+  })
 
-const departmentResponse = await getDepartment(props.employee.departmentId)
-const department = ref(departmentResponse)
+  //const departmentResponse = await getDepartment(props.demon.departmentId)
+  //const department = ref(departmentResponse)
 </script>
 
 <template>
+  <RouterLink v-if="props.villain.villainID" :to="`/api/demons/${props.villain.villainID}`">
   <div class="card" @click="selectCard">
     <div class="card-image">
-      <img :src="faker.internet.avatar()" alt="" srcset="" />
+      <img :src="props.villain.image" alt="" srcset="" />
     </div>
     <div class="card-details">
-      <p class="card-details-name">{{ props.employee.name }}</p>
-      <p class="card-details-job">{{ props.employee.title }}, {{ department.name }}</p>
-      <p class="card-details-quote">"{{ props.employee.quote }}"</p>
+      <p class="card-details-name font-poppins">{{ props.villain.name }}</p>
+
     </div>
   </div>
+  </RouterLink>
 </template>
 
 <style scoped lang="postcss">
-.card {
-  @apply cursor-pointer overflow-hidden rounded-md bg-slate-100 p-8 shadow-md transition-transform duration-300 hover:scale-110 hover:shadow-2xl hover:shadow-slate-900;
-
-  &-image {
-    img {
-      @apply mx-auto rounded-full object-contain;
+  .card {
+    @apply cursor-pointer overflow-hidden rounded-md p-8 shadow-md transition-transform duration-300 hover:scale-110 hover:shadow-2xl hover:shadow-slate-900;
+    &-image {
+      img {
+        @apply mx-auto rounded-full object-contain;
+      }
+    }
+    &-details {
+      @apply flex flex-col gap-2  pt-6 text-center;
+      &-name {
+        @apply text-4xl  text-black;
+      }
+      &-job {
+        @apply -mt-2 text-xs font-bold text-yellow-700;
+      }
+      &-quote {
+        @apply pt-4 text-lg italic text-slate-800;
+      }
     }
   }
-
-  &-details {
-    @apply flex flex-col gap-2 pt-6 text-center;
-
-    &-name {
-      @apply text-3xl font-thin tracking-wider text-slate-800;
-    }
-
-    &-job {
-      @apply -mt-2 text-xs font-bold text-yellow-700;
-    }
-
-    &-quote {
-      @apply pt-4 text-lg italic text-slate-800;
-    }
-  }
-}</style>
+</style>

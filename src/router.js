0694def2e30b5a74a1575054f8ab02ce3a6be34c
Julian Fragoso <starsmember@yahoo.com>
2023-12-05 21:32:46 -0600

    Added HomePage

--- src/router.js
+++ src/router.js
@@ -18,16 +18,17 @@ const routes = [
 
 import { createRouter, createWebHistory } from 'vue-router'
 
-import { useAuth } from '@/composables/useAuth' 
+import { useAuth } from '@/composables/useAuth'
 const { isAuthenticated } = useAuth()
 
 import MainPage from '@/components/MainPage.vue'
 import LoginPage from '@/components/LoginPage.vue'
 import SettingsPage from '@/components/SettingsPage.vue'
 import NotFound from '@/components/NotFound.vue'
+import HomePage from '@/views/HomePage.vue'
 
 const routes = [
-  { path: '/Vue-Company-Directory/', name: 'Home', component: MainPage }, // <--- Change path name to "Vue-Company-Directory"
+  { path: '/Final.Project.Website/', name: 'Home', component: MainPage }, // <--- Change path name to "Final.Project.Website"
   { path: '/login', name: 'Login', component: LoginPage },
   { path: '/settings', name: 'Settings', component: SettingsPage, meta: { requiresAuth: true } },
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },

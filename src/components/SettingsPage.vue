832e03dc275489ca9106cf3cb00376ec305e4710
Julian Fragoso <starsmember@yahoo.com>
2023-12-06 01:01:27 -0600

    added settings

--- src/components/SettingsPage.vue
+++ src/components/SettingsPage.vue
@@ -1,3 +1,40 @@
+<script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  auth: {
+    username: import.meta.env.VITE_API_USERNAME,
+    password: import.meta.env.VITE_API_PASSWORD,
+  },
+})
+
+
+const villainName = ref('')
+const villainImage = ref('')
+  
+const addVillain = async () => {
+  const { data } = await api.post('/api/villains', {
+    name: villainName.value,
+    image: villainImage.value
+  })
+}
+</script>
+
 <template>
-    <h1>Settings Page</h1>
+  <form class="login-form" @submit.prevent="addVillain">
+    <input v-model="villainName" type="text" placeholder="name" />
+    <input v-model="villainImage" type="text" placeholder="image" />
+    <button type="submit" class="bg-green-500 px-4 py-2">submit</button>
+  </form>
 </template>
+
+<style scoped lang="postcss">
+  .login-form {
+    @apply mx-auto mt-80 flex max-w-md flex-col gap-4 rounded-md bg-white p-8 shadow-lg;
+    & input {
+      @apply rounded-md px-4 py-2 text-xl ring-1 ring-slate-300;
+    }
+  }
+</style>

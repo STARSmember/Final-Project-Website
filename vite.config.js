8c1ef36be839c627c642f33096f8eade9055bd4c
Julian Fragoso <starsmember@yahoo.com>
2023-12-11 16:09:43 -0600

    added final project website

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Final.Project.Website/',   // <---- added "base: /'Vue-Company-Directoy/" <-- don't forget the name inside single quotes
+  base: '/Final-Project-Website/',   // <---- added "base: /'Vue-Company-Directoy/" <-- don't forget the name inside single quotes
   plugins: [vue()],
   resolve: {
     alias: {

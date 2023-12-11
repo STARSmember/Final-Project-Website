f99d58249004e3e2a9a37c538f56d4fca7a126f1
Julian Fragoso <starsmember@yahoo.com>
2023-12-11 13:32:56 -0600

    added changes

--- src/router.js
+++ src/router.js
@@ -25,13 +25,16 @@ import MainPage from '@/components/MainPage.vue'
 import LoginPage from '@/components/LoginPage.vue'
 import SettingsPage from '@/components/SettingsPage.vue'
 import NotFound from '@/components/NotFound.vue'
-import HomePage from '@/views/HomePage.vue'
+//import HomePage from '@/views/HomePage.vue'
+
 
 const routes = [
-  { path: '/Final.Project.Website/', name: 'Home', component: MainPage }, // <--- Change path name to "Final.Project.Website"
+  { path: '/Final-Project-Website/', name: 'Home', component: MainPage }, // <--- Change path name to "Final.Project.Website"
   { path: '/login', name: 'Login', component: LoginPage },
   { path: '/settings', name: 'Settings', component: SettingsPage, meta: { requiresAuth: true } },
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
+  { path: '/other', name: 'Other', component: () => import('@/view/OtherPage.vue') },
+  { path: '/api/villains/:id', name: 'Villain', component: () => import('/@views/VillainPage.vue')},
 ]
 
 const router = createRouter({
